38738e0ddcf987d8672aa39ab694cda484dd949a
Katya Ivanova <ivanova49249@mail.ru>
2023-09-01 00:08:11 +0700

    homework_2.1

--- src/item.py
+++ src/item.py
@@ -1,9 +1,13 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    keep = True
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,11 +17,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-        Item.all.append(self)
+        if Item.keep:
+            Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, line):
+        if len(line) > 10:
+            self.__name = line[:10]
+            raise Exception("Длина наименования товара превышает 10 символов")
+        else:
+            self.__name = line
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +49,15 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @staticmethod
+    def string_to_number(string: str) -> int:
+        return int(float(string))
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        Item.all.clear()
+        with open(filename, 'r', encoding="windows-1251") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item(row["name"], float(row["price"]), int(row["quantity"]))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
+
+
+@pytest.fixture()
+def dummy_item():
+    return Item("", 0, 0)
 
 
 def test_calculate_total_price():
@@ -15,3 +22,24 @@ def test_apply_discount():
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 4000
+
+
+def test_name(dummy_item):
+    dummy_item.name = "Магнитофон"
+    assert dummy_item.name == "Магнитофон"
+
+
+def test_name_raise_exception(dummy_item):
+    with pytest.raises(Exception):
+        dummy_item.name = "Компьютерная мышь"
+
+
+def test_string_to_number():
+    assert Item.string_to_number("7") == 7
+    assert Item.string_to_number('5.0') == 5
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    assert type(Item.all[0]) is Item
